1415e5bef4cd9e58ca3d4af54673250a645fc8e1
AlexandrePaul2003 <alexandre.paul@condorcet.be>
2023-09-28 14:04:47 +0200

    Added classes

---
+++ Movies/Movies/Models/Personne.cs
@@ -0,0 +1,23 @@
+﻿namespace Movies.Models
+{
+    public abstract class Personne
+    {
+        public string nom;
+        public string prenom;
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        public Personne(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+    }
+}
